f16baae320296547f2127f62b5817b10042b929a
Bhavya Jain <bhavya.j360@gmail.com>
2026-02-15 21:10:06 +0530

    Resume Fix

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
         {/* Resume Button */}
         <div>
           <a
-            href="public/resume.pdf"
+            href="/resume.pdf"
             target="_blank"
             className="inline-block  font-medium bg-button-bg px-6 py-3 rounded-lg hover:bg-blue-700 transition"
           >
--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
       <div className="max-w-6xl mx-auto flex flex-col items-center space-y-12 px-6">
         <div>
           <a
-            href="public/resume.pdf"
+            href="/resume.pdf"
             target="_blank"
             className="inline-block font-medium bg-button-bg px-6 py-3 rounded-lg hover:bg-blue-700 transition"
           >
